79a35d59b6180555a680ba24c2927ac4a5756af8
phanminhquang892-hash <phanminhquang892@gmail.com>
2026-02-25 07:47:38 +0700

    Add files via upload

---
+++ thuchanh/index.js
@@ -0,0 +1,21 @@
+let orders = ["Đơn hàng A", "Đơn hàng B", "Đơn hàng C", "Đơn hàng D", "Đơn hàng E"];
+let revenues = [1500, 2800, 1200, -500, 3200];
+
+function TotalAmount(revenues) {
+    let hasNegative = revenues.some(revenue => revenue < 0);
+    let allAbove = revenues.every(revenue => revenue > 500);
+
+    console.log("Có đơn hàng âm:", hasNegative);
+    console.log("Tất cả trên 500:", allAbove);
+}
+
+console.log(TotalAmount(revenues));
+
+function profit(revenues) {
+    let result = revenues.map(revenue => revenue * 0.9);
+    console.log("Lợi nhuận ròng:", result);
+    return result;
+}
+let total = profit(revenues);
+
+
